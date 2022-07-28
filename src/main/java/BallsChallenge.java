3ce91e0304ebf07168af00b784b7a691a84dd69a
VarnikaB <1rn18cs115.varnika@gmail.com>
2022-07-28 14:06:52 +0530

    Begin project

---
+++ src/main/java/BallsChallenge.java
@@ -0,0 +1,8 @@
+import processing.core.PApplet;
+
+public class BallsChallenge extends PApplet {
+
+    public static void main(String[] args){
+        PApplet.main("BallsChallenge", args);
+    }
+}
